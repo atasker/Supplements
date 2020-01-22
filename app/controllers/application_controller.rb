9e248a943f790e47b30ef6b21b1f7ecdbf9b2ccf
Angus Tasker <atasker2@gmail.com>
2020-01-22 21:46:29 +0100

    fixed searchbar to work sitewide

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,17 @@
 class ApplicationController < ActionController::Base
+
+	before_action :set_searchable
+
+	private
+
+	# Creates an array of all supplement names for use in the
+	# typeahead search bar in sitewide header - views/layouts/_header
+
+	def set_searchable
+	  gon.supplement_array = []
+	  Supplement.all.each do |supplement|
+	    gon.supplement_array << supplement.name
+	  end
+	end
+
 end
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,10 +1,6 @@
 class WelcomeController < ApplicationController
   
   def index
-  	gon.supplement_array = []
-  	Supplement.all.each do |supplement|
-  		gon.supplement_array << supplement.name
-  	end
   end
 
 end
