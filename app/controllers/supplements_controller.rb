48da04a14c9f43900ef302b2c1d90e3d958a5f30
Angus Tasker <atasker2@gmail.com>
2020-01-28 11:48:00 +0000

    changed breadcrumb navigation order

--- app/controllers/supplements_controller.rb
+++ app/controllers/supplements_controller.rb
@@ -2,10 +2,12 @@ class SupplementsController < ApplicationController
 
 	def index
 		@supplements = Supplement.all
+		@categories = Category.all
 	end
 
 	def show
 		@supplement = Supplement.friendly.find(params[:id])
+		@category = Category.find(@supplement.category_id)
 	end
 
 end
