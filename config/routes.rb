bb59c7742fae74465e89e4db2f7fb10533dc314a
Angus Tasker <atasker2@gmail.com>
2020-01-31 14:56:27 +0000

    updated routes for succinct contact url

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
 
   get 'static_pages/home'
-  get 'static_pages/contact'
   root 'static_pages#home'
+  get 'contact' => 'static_pages#contact', :as => :contact
 
   namespace :admin do
       resources :categories
