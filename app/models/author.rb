e90a0d31fc8f4b6ed0c6c21a7ff8d88e7302aea7
Angus Tasker <atasker2@gmail.com>
2020-02-02 18:10:52 +0000

    comments

--- app/models/author.rb
+++ app/models/author.rb
@@ -2,6 +2,7 @@ class Author < ApplicationRecord
 
 	has_many :articles
 	has_many :supplements
+	#has_many :reviewers, class_name: "Author", foreign_key: "reviewer_id"
 
 	validates :name, presence: true
 	validates :title, presence: true
