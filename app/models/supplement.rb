7aa19203b84d813a69062d1308f2288e7c2dd55d
Angus Tasker <atasker2@gmail.com>
2020-02-03 00:59:52 +0000

    removed comments

--- app/models/supplement.rb
+++ app/models/supplement.rb
@@ -7,8 +7,7 @@ class Supplement < ApplicationRecord
 
 	belongs_to :category
 	belongs_to :author
-	# TODO: Add reviewer_id to supplements, pointing to author table
-	# belongs_to :reviewer, class_name: "Author", foreign_key: :reviewer_id
+	
 	validates :name, presence: true
 
 end
