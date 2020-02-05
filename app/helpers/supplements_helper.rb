08da4c623fe2ab84bcd7b5a666f0fb922e89fda5
Angus Tasker <atasker2@gmail.com>
2020-02-05 11:10:10 +0000

    add helper methods for string to hash/array

--- app/helpers/supplements_helper.rb
+++ app/helpers/supplements_helper.rb
@@ -1,2 +1,13 @@
 module SupplementsHelper
+
+  def string_to_hash(string)
+    hash = eval(string)
+    return hash
+  end
+
+  def string_to_array(string)
+    array = string.split(',')
+    return array
+  end
+
 end
